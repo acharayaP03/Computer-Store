080ec819781d7ed6e25378919861a19abf594a27
acharayaP03 <acharyaP03@gmail.com>
2024-11-05 17:41:01 +1100

    some json data has null on date

--- Models/Computer.cs
+++ Models/Computer.cs
@@ -7,7 +7,7 @@ public class Computer
     public bool HasWifi { get; set; }
     public bool HasLTE { get; set; }
     public string VideoCard { get; set; } = "";
-    public DateTime ReleaseDate { get; set; }
+    public DateTime? ReleaseDate { get; set; }
     public decimal Price { get; set; }
 
 
